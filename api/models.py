96a85992144f381de8b09e26eb40a4537535a5c1
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-12-13 10:13:42 -0200

    Adicionado model Motorista
    Model Motorista implementado e adicionado ao painel de Admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Motorista(models.Model):
+    nome = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11) # TODO como adicionar máscara? (AAA.BBB.CCC-DD)
+    celular = models.CharField(max_length=11) # TODO como adicionar máscara? ( (AA) BCCCC-DDDD )
+    # TODO campo endereço de mensagem instantânea: tipo int?
+    cnh = models.CharField(max_length=11) # TODO confirmar que CNH só tem 11 dígitos
+    pontuacao = models.FloatField()
+
+    # TODO plain-text password storing -> really bad idea
+    # Check [Password management in Django](https://docs.djangoproject.com/en/1.9/topics/auth/passwords/)
+    senha = models.CharField(max_length=8) # TODO confirmar requisito: qual o tamanho máximo da senha?
+   
+
